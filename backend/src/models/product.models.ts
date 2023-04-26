a081c1ae130f2f54ea6d8767e323c2498f3f013b
RohanSasne <rohansasne30@gmail.com>
2023-04-26 13:29:26 +0530

    add Product backend

---
+++ backend/src/models/product.models.ts
@@ -0,0 +1,9 @@
+export type IProductType = {
+    product_id?: number,
+    product_description: string,
+    category: string,
+    quantity: number,
+    price: number,
+    product_name: string,
+    supplier_id?: number,
+}
